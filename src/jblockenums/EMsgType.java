5266d7d60059b4ebc22043c6993bc84985cb2331
Adrian Harwood <adrian.harwood@manchester.ac.uk>
2018-07-16 15:30:07 +0100

    Some clean up after code review

---
+++ src/jblockenums/EMsgType.java
@@ -0,0 +1,5 @@
+package jblockenums;
+
+public enum EMsgType
+{
+}
--- src/jblockmain/Prompts.java
+++ src/jblockmain/Prompts.java
@@ -2,8 +2,16 @@ package jblockmain;
 
 import javax.swing.*;
 
+/**
+ * Class to wrap various message box initialisations.
+ */
 public class ErrorPrompt
 {
+    /**
+     * Create a message box.
+     * @param infoMessage   Body text of box.
+     * @param titleBar      Error title.
+     */
     public static void infoBox(String infoMessage, String titleBar)
     {
         JOptionPane.showMessageDialog(null, infoMessage, "Error: " + titleBar, JOptionPane.INFORMATION_MESSAGE);
