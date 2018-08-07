2bdfd43f9178a22754c3151b1f88446800196827
jamesgill1997 <james.gill-6@student.manchester.ac.uk>
2018-08-07 16:53:20 +0100

    More work towards a working rectangle plot, last bit needed is to just finish how the four points are passed to the dxfwriter and the layered option

--- src/analysis/RectanglePlot.java
+++ src/analysis/RectanglePlot.java
@@ -1,9 +1,14 @@
 package analysis;
 
+import dxfwriter.DxfFile;
+import jblockenums.EAnalysis;
 import jblockmain.IPlottable;
 import jblockmain.Measurements;
 
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 
 public class RectanglePlot implements IPlottable
@@ -12,6 +17,14 @@ public class RectanglePlot implements IPlottable
 
     // List of Rectangles in this plot
     private ArrayList<Rectangle> rectangles;
+
+    // Enum for analysis type
+    private final EAnalysis analysis = EAnalysis.RECTANGLEPLOT;
+
+    // Variables for x,y axis measurement ID
+    private int xAxisID;
+    private int yAxisID;
+    private String userID;
 
     // Constructor
     public RectanglePlot()
@@ -31,29 +44,48 @@ public class RectanglePlot implements IPlottable
                         measurements.getId(measurementIdY).value
                 )
         );
+        xAxisID = measurementIdX;
+        yAxisID = measurementIdY;
+        userID = measurements.getName();
+    }
+
+    /* Interface implementation */
+    private void rangeCheck(int blockNumber)
+    {
+        if (blockNumber > rectangles.size()) throw new IndexOutOfBoundsException("Accessing out of range of number of blocks!");
     }
 
     @Override
     public ArrayList<Double> getXPoints(int blockNumber) throws Exception
     {
+        rangeCheck(blockNumber);
         return rectangles.get(blockNumber).getX();
     }
 
     @Override
     public ArrayList<Double> getYPoints(int blockNumber) throws Exception
     {
+        rangeCheck(blockNumber);
         return rectangles.get(blockNumber).getY();
     }
 
     @Override
     public ArrayList<Double> getXCtPoints(int blockNumber) throws Exception
     {
+        rangeCheck(blockNumber);
         return null;
     }
 
     @Override
     public ArrayList<Double> getYCtPoints(int blockNumber) throws Exception
     {
+        rangeCheck(blockNumber);
+        return null;
+    }
+
+    private ArrayList<String> getNames(int blockNumber) throws IndexOutOfBoundsException
+    {
+        rangeCheck(blockNumber);
         return null;
     }
 
@@ -66,6 +98,33 @@ public class RectanglePlot implements IPlottable
     @Override
     public void writeToDXF(File fileOutput, boolean[] dxfLayerChooser)
     {
-        // TODO
+        for (int i = 0; i < rectangles.size(); i++)
+        {
+            // Construct output path
+            Path path = Paths.get(fileOutput.toString() + "/" + analysis + "/");
+
+            // Create directory structure if required
+            try
+            {
+                Files.createDirectories(path);
+            }
+            catch (java.io.IOException e)
+            {
+                System.err.println("Cannot create directories - " + e);
+            }
+
+            // Create new DXF file
+            String filename = userID + "_" + xAxisID + "_" + yAxisID + "_Rectangle_Plot";
+            DxfFile file = new DxfFile(path.toString() + "/" + filename);
+            try
+            {
+                file.addLines(getXPoints(i), getYPoints(i));
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+            file.writeFile(filename, dxfLayerChooser);
+        }
     }
 }
