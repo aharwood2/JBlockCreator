f5838d3cfeebc875d260b06e7815ea3a7b95392d
mazdo25 <muad-619@hotmail.com>
2019-08-13 22:32:52 +0100

    added most of the sleeve, just need to fix circular curves

--- src/gill/SweatShirtPattern.java
+++ src/gill/SweatShirtPattern.java
@@ -121,7 +121,9 @@ public class SweatShirtPattern extends Pattern
         blocks.add(backBlock);
 
         // Conditional to set a position based on large of 2 values
-        double shoulderDepthXPosition = LeftShoulderDrop > RightShoulderDrop ? LeftShoulderDrop : RightShoulderDrop;
+        double largestShoulderDrop = LeftShoulderDrop > RightShoulderDrop ? LeftShoulderDrop : RightShoulderDrop;
+        double largestArmLength = ArmLengthLeft > ArmLengthRight ? ArmLengthLeft : ArmLengthRight;
+        double largestWristCircum = WristCircumL > WristCircumR ? WristCircumL : WristCircumR;
 
         // All of the keypoints added in as Vectors
         Vector2D point1 = new Vector2D(CBNeckDepthDefault, 0.0);
@@ -130,7 +132,7 @@ public class SweatShirtPattern extends Pattern
         Vector2D point4 = new Vector2D(HalfBackCentreTapeMeasure, OptionalSmallBackWaistTapeMeasure / 4.0 + OptionalSoBWaistCircEase / 4.0);
         Vector2D point5 = new Vector2D(ScyeDepth + CBToUnderArmEase, point3.getY());
         Vector2D point6 = new Vector2D(ScyeDepth / 2.0 + CBToUnderArmEase, BackShoulderWidthHorizontal / 2.0 - 2.0);
-        Vector2D point7 = new Vector2D(shoulderDepthXPosition, BackShoulderWidthHorizontal / 2.0);
+        Vector2D point7 = new Vector2D(largestShoulderDrop, BackShoulderWidthHorizontal / 2.0);
         Vector2D point8 = new Vector2D(0.0, MidNeckBaseWidth / 2.0 + NeckWidthEase / 2.0);
 
         // All the Keypoints added to the block
@@ -148,9 +150,11 @@ public class SweatShirtPattern extends Pattern
         backBlock.addDirectedCurve(point6, point7, new Vector2D(-1.0, 0.0), new Vector2D(point8.subtract(point7)), new double[] {0.0, 90.0});
         backBlock.addDirectedCurve(point8, point1, new double[] {90.0,90.0});
 
+        // Front Sweatshirt Block
         Block frontBlock = new Block(userName + "_Gill_Front_SweatShirt_Block");
         blocks.add(frontBlock);
 
+        // Most of the keypoints added
         Vector2D point9 = new Vector2D(NeckCircumference / 5.0 - 1.5, 0);
         Vector2D point10 = new Vector2D(point2.getX(), 0);
         Vector2D point11 = new Vector2D(point2.getX(), point3.getY());
@@ -160,6 +164,7 @@ public class SweatShirtPattern extends Pattern
         Vector2D point15 = point7;
         Vector2D point16 = point8;
 
+        // Addition of all the points as keypoints
         frontBlock.addKeypoint(point9);
         frontBlock.addKeypoint(point10);
         frontBlock.addKeypoint(point11);
@@ -171,13 +176,53 @@ public class SweatShirtPattern extends Pattern
         frontBlock.addQuadraticBezierCurve(point13, new Vector2D(point13.getX(), point14.getY()), point14);
         frontBlock.addDirectedCurve(point14, point15, new Vector2D(-1.0, 0.0), new Vector2D(point16.subtract(point15)), new double[] {0.0, 90.0});
         frontBlock.addDirectedCurve(point16, point9, new double[] {90.0,90.0});
+
+        Block sleeveBlock = new Block(userName + "_Gill_Sleeve_Block");
+        blocks.add(sleeveBlock);
+
+        // Calculates the length between point 13 and point 15 and point 1 to point5 but only in X
+        double sleeveWidthAid = new Vector2D(point13.subtract(point15)).norm();
+        double crownHeightAid = point5.getX() - point1.getX();
+
+        // Calculation of point1
+        Vector2D sleevePoint9 = new Vector2D(-(point5.getX() / 2.0), 0.0);
+        Vector2D sleevePoint1 = new Vector2D(0.0, -(Math.sqrt(Math.pow(crownHeightAid + 2.0, 2) - Math.pow(sleevePoint9.getX() / 2.0, 2))));
+        Vector2D sleevePoint3 = new Vector2D(largestArmLength - (sleevePoint9.getX() / 2.0), -((largestWristCircum + WristEase) / 2.0));
+        Vector2D sleevePoint4 = new Vector2D(sleevePoint3.getX(), 0.0);
+        Vector2D sleevePoint5 = new Vector2D(sleevePoint3.getX(), -(sleevePoint3.getY()));
+        Vector2D sleevePoint7 = new Vector2D(0.0, -(sleevePoint1.getY()));
+
+        // Create a direction vector from point1 to point3
+        Vector2D D1_3 = new Vector2D(sleevePoint3.subtract(sleevePoint1));
+        // Calculate point 2 as halfway
+        Vector2D sleevePoint2 = new Vector2D(sleevePoint1.add(D1_3.divide(2.0)));
+        // Point6 is just point 2 reflected in the x-axis
+        Vector2D sleevePoint6 = new Vector2D(sleevePoint2.getX(), -(sleevePoint2.getY()));
+
+        Vector2D D7_8 = new Vector2D(sleevePoint9.subtract(sleevePoint7));
+        // Point8 is just 1/3rd the distance of 7 to 9
+        Vector2D sleevePoint8 = new Vector2D(sleevePoint7.add(D7_8.divide(3.0)));
+        Vector2D sleevePoint10 = new Vector2D(sleevePoint8.getX(), -(sleevePoint8.getY() - 0.01));
+
+        sleeveBlock.addKeypoint(sleevePoint1);
+        sleeveBlock.addKeypoint(sleevePoint2);
+        sleeveBlock.addKeypoint(sleevePoint3);
+        sleeveBlock.addKeypoint(sleevePoint4);
+        sleeveBlock.addKeypoint(sleevePoint5);
+        sleeveBlock.addKeypoint(sleevePoint6);
+        sleeveBlock.addKeypoint(sleevePoint7);
+        sleeveBlock.addKeypoint(sleevePoint8);
+        sleeveBlock.addKeypoint(sleevePoint10);
+
+        sleeveBlock.addCircularCurve(sleevePoint8, sleevePoint10, point9.getX(), true);
+
     }
 
     protected static ArrayList<easeMeasurement> easeMeasurements = new ArrayList<>();
 
     public static void populateEaseMeasurements()
     {
-        // Check to see it hasn't already been populated
+        // Check to see it hasn't already been populated / it is empty
         if (easeMeasurements.size() > 0) {return;}
         easeMeasurements.add(new easeMeasurement("Bust/Chest Ease", 16));
         easeMeasurements.add(new easeMeasurement("Back Width Ease", 2.5));
