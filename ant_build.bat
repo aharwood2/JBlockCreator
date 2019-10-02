f057145b7c109fd223c036aa0f82d22d8c0d853b
Adrian Harwood <adrian.harwood@manchester.ac.uk>
2019-10-02 12:45:11 +0100

    Updated build file to latest version of JDK

--- ant_build.bat
+++ ant_build.bat
@@ -5,7 +5,7 @@ REM <---- Set your Ant path here ---->
 set ANT=C:\Temp\apache-ant-1.10.5
 
 REM <---- Set your JDK path here ---->
-set "JAVA_HOME=C:\Program Files\Java\jdk1.8.0_201"
+set "JAVA_HOME=C:\Program Files\Java\jdk1.8.0_221"
 
 REM <---- Don't change these bits unless you know what you are doing ---->
 set ANT_BIN=%ANT%\bin
