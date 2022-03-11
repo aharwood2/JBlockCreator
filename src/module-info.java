2a80c77ec6518b9506b5899f0c8a6a5c9227d9f9
Adrian Harwood <adrian.harwood@manchester.ac.uk>
2022-03-11 17:20:33 +0000

    Added new dependencies and updated Ant build configuration

--- src/module-info.java
+++ src/module-info.java
@@ -1,7 +1,6 @@
 module JBlockCreator {
     requires javafx.controls;
     requires javafx.fxml;
-
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires org.kordamp.ikonli.javafx;
