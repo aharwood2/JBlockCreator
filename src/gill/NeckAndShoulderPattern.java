c0d139acf1818dd65378c1af97732c7ef2d4825a
Daniel Djesa <danieldjesa2006@outlook.com>
2022-08-03 15:43:58 +0100

    Finished off neck and shoulder pattern

--- src/gill/NeckAndShoulderPattern.java
+++ src/gill/NeckAndShoulderPattern.java
@@ -4,6 +4,8 @@ import jblockenums.EPattern;
 import jblockmain.*;
 import mathcontainers.Vector2D;
 
+import static java.lang.Math.abs;
+
 public class NeckAndShoulderPattern extends Pattern
 {
     public NeckAndShoulderPattern(String userName, InputFileData dataStore, MeasurementSet template)
@@ -25,11 +27,12 @@ public class NeckAndShoulderPattern extends Pattern
         measurements.addMeasurement(new Measurement("backNeckDiagonal", "A74"));
 
         // TODO: Daniel to finish
-        measurements.addMeasurement(new Measurement("A", "A93"));
-        measurements.addMeasurement(new Measurement("B", "A95"));
-        measurements.addMeasurement(new Measurement("C", "A96"));
-        measurements.addMeasurement(new Measurement("D", "A97"));
-        measurements.addMeasurement(new Measurement("E", "A98"));
+        measurements.addMeasurement(new Measurement("NeckBaseWidth", "A93"));
+        measurements.addMeasurement(new Measurement("ShoulderLengthLeft", "A94"));
+        measurements.addMeasurement(new Measurement("LShoulderPointZ", "A95"));
+        measurements.addMeasurement(new Measurement("RShoulderPointZ", "A96"));
+        measurements.addMeasurement(new Measurement("LSideNeckPointZ", "A97"));
+        measurements.addMeasurement(new Measurement("RSideNeckPointZ", "A98"));
     }
 
     @Override
@@ -41,18 +44,52 @@ public class NeckAndShoulderPattern extends Pattern
         var backNeckDiagonal = get("backNeckDiagonal");
 
         // TODO: Daniel to finish parsing measurements into local variables
+        var NeckBaseWidth = get("NeckBaseWidth");
+        var ShoulderLengthLeft = get("ShoulderLengthLeft");
+        var LShoulderPointZ = get("LShoulderPointZ");
+        var RShoulderPointZ = get("RShoulderPointZ");
+        var LSideNeckPointZ = get("LSideNeckPointZ");
+        var RSideNeckPointZ = get("RSideNeckPointZ");
 
         // Build block
-        Block block = new Block(userName + "_Gill_NeckShoulder");
-        blocks.add(block);
+        Block fullBlock = new Block(userName + "_Gill_NeckShoulder");
+        blocks.add(fullBlock);
 
-        // Add point 1
+        //If conditions
+        double Rsidepoint = ((RSideNeckPointZ < 0 &&  RShoulderPointZ < 0) || (RSideNeckPointZ > 0 &&  RShoulderPointZ < 0)) ? abs(RSideNeckPointZ - RShoulderPointZ) : -abs(RSideNeckPointZ - RShoulderPointZ);
+        double Lsidepoint = ((LSideNeckPointZ < 0 &&  LShoulderPointZ < 0) || (LSideNeckPointZ > 0 &&  LShoulderPointZ < 0)) ? abs(LSideNeckPointZ - LShoulderPointZ) : -abs(LSideNeckPointZ - LShoulderPointZ);
+
+        // Defining each point
         var point1 = new Vector2D(0, backNeckDiagonal);
-        block.addKeypoint(point1);
-        var point2 = new Vector2D(0, frontNeckDiagonal);
-        block.addKeypoint(point2);
 
         // TODO: Daniel to finish
+        var point2 = new Vector2D(0, -frontNeckDiagonal);
+        var point3 = new Vector2D(-(NeckBaseWidth*0.5), 0);
+        var point4 = new Vector2D(-(NeckBaseWidth*0.5 + shoulderLengthRight), Rsidepoint);
+        var point5 = new Vector2D(NeckBaseWidth*0.5, 0);
+        var point6 = new Vector2D(NeckBaseWidth*0.5 + ShoulderLengthLeft, Lsidepoint);
+
+
+        // All the keypoints/vectors added
+        fullBlock.addKeypoint(point1);
+        fullBlock.addCircularArc(point1, point3, 0.9, true);
+        fullBlock.addKeypoint(point3);
+        fullBlock.addCircularArc(point3, point2, 1.5, true);
+        fullBlock.addKeypoint(point2);
+        fullBlock.addCircularArc(point2, point5, 1.5, true);
+        fullBlock.addKeypoint(point5);
+        fullBlock.addCircularArc(point5, point1, 0.9, true);
+        fullBlock.addKeypoint(point1);
+        fullBlock.addKeypoint(point4);
+        fullBlock.addKeypoint(point2);
+        fullBlock.addKeypoint(point6);
+        fullBlock.addKeypoint(point5);
+        fullBlock.addKeypoint(point3);
+        fullBlock.addKeypoint(point4);
+        fullBlock.addKeypoint(point1);
+        fullBlock.addKeypoint(point2);
+        fullBlock.addKeypoint(point1);
+        fullBlock.addKeypoint(point6);
 
     }
 }
